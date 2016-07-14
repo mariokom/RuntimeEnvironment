f90000447c81acc70d6f241da8794f3005be8b16
mariokom <mkomod05@cs.ucy.ac.cy>
2016-07-14 18:07:27 +0300

    Updated auto-conf file

--- GPII/auto-conf.bat
+++ GPII/auto-conf.bat
@@ -1,5 +1,9 @@
+cd ..
+
 SET pathSingle=%CD%
 
 SET pathDouble=%pathSingle:\=\\%
+
+cd GPII
 
 powershell -Command "(gc win32.json) -replace '<rootFolderPath>', '%pathDouble%' | Out-File win32.json"
