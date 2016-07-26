bef242bb7220836a9a92fb2397ba7f5b27b43d6d
mariokom <mkomod05@cs.ucy.ac.cy>
2016-07-26 12:45:13 +0300

    Set encoding in auto-conf file

--- GPII/auto-conf.bat
+++ GPII/auto-conf.bat
@@ -6,4 +6,4 @@ SET pathDouble=%pathSingle:\=\\%
 
 cd GPII
 
-powershell -Command "(gc win32.json) -replace '<rootFolderPath>', '%pathDouble%' | Out-File win32.json"
+powershell -Command "(gc win32.json) -replace '<rootFolderPath>', '%pathDouble%' | Out-File win32.json -encoding ASCII"
